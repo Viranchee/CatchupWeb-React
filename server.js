d617feb2dac65c2bcd14492e067201d8adfd11e0
Omkar Agrawal <omkar3654@gmail.com>
2019-03-15 07:03:14 +0530

    minor changes

---
+++ server.js
@@ -0,0 +1,65 @@
+"use strict";
+
+const express = require('express');
+const morgan = require('morgan');
+const http = require('http');
+const https = require('https');
+const path = require('path');
+const axios = require('axios');
+const bodyParser = require('body-parser');
+const helmet = require('helmet');
+
+//------------------------------------------------------------------------------------------------------
+//                    To frequent use constants
+//------------------------------------------------------------------------------------------------------
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+//------------------------------------------------------------------------------------------------------
+//                    For adding functionalities to express
+//------------------------------------------------------------------------------------------------------
+
+app.use(morgan('combined'));
+app.use(helmet());
+app.use(bodyParser.json());
+app.use(bodyParser.text({type: "text/xml"}));
+app.listen(port, () => {console.log(`Our site is hosted on ${port}! If you donot know to open just go to browser and type (localhost:${port})`);
+});
+
+//------------------------------------------------------------------------------------------------------
+//                    For routes
+//------------------------------------------------------------------------------------------------------
+//--------------------For GET Requests------------------------------------------------------------------
+app.get('/hackernews', (req, res) => {
+    axios.get("https://hnrss.org/newest.jsonfeed", {responseType: 'json'})
+        .then(resp => {
+            res.send(resp.data);
+        })
+        .catch(err => {
+            console.log(`\n\n ${err} \n\n`);
+            res.status(500).send(err);
+        });
+});
+
+app.get('/hackernewsTop', (req, res) => {
+    axios.get("https://hacker-news.firebaseio.com/v0/topstories.json", {responseType: 'json'})
+        .then(resp => {
+            res.send(resp.data);
+        })
+        .catch(err => {
+            console.log(`\n\n ${err} \n\n`);
+            res.status(500).send(err);
+        });
+});
+
+app.get('/hackerearth', (req, res) => {
+    axios.get("http://engineering.hackerearth.com/atom.xml", {responseType: 'document'})
+        .then(resp => {
+            res.send(resp.data);
+        })
+        .catch(err => {
+            console.log(`\n\n ${err} \n\n`);
+            res.status(500).send(err);
+        });
+});
--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, { Component, createRef } from 'react';
+import React, { PureComponent } from 'react';
 import Table from "./Table.js";
+import axios from 'axios'
 
 import './App.css';
 
-class App extends Component {
-    constructor() {
+class App extends PureComponent {
+    /* constructor() {
         super();
         this.state = {
             hnNews: undefined ,
@@ -16,17 +17,62 @@ class App extends Component {
         this.changeSort = this.changeSort.bind(this);
         this.sort = createRef();
         this.countClick = 0;
-    }    
-    
-    gethnNews () {
-
+    } */    
+    state = {
+        hnNews: undefined ,
+            hnNewsTop: undefined,
+            hackerEarth: undefined,
+            data: undefined
     }
 
-    loadData() {
-        console.log(window.location.hash.split('#')[1]);
+    gethnNews = () => {
+        axios.get('http://cors.io/?https://hnrss.org/newest.jsonfeed', {
+            // axios.get('/hackernews',{
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Method': 'GET'
+                  },
+                  responseType: 'json'
+        })
+        .then(response => {
+            this.setState({hnNews: response.data});
+        })
+        .then(() => {
+            console.log(this.state.hnNews);
+        })
+        .catch(err => {
+            console.log(err);
+        });
     }
 
-    changeSort (event) {
+    getHnnewsTop = () => {
+        axios.get('https://hacker-news.firebaseio.com/v0/topstories.json', {
+                    responseType: 'json'
+                })
+                .then(response => {
+                    this.setState({hnNewsTop: response.data});
+                })
+                .then(() => {
+                    console.log(this.state.hnNewsTop);
+                })
+    }
+
+    loadData = () => {
+        switch(window.location.hash.split('#')[1]) {
+            case 'hackernewsTop' :
+                this.getHnnewsTop();
+                break;
+            case 'hackernews' :
+                this.gethnNews();
+                break;
+
+            default: alert("Error in loadData")
+        }
+        
+        // console.log(window.location.hash.split('#')[1]);
+    }
+
+    changeSort = (event) => {
         this.countClick++;
         if (this.countClick % 2 === 0) {
             window.location.hash = "hackernewsTop";
@@ -37,7 +83,7 @@ class App extends Component {
         }
     }
 
-    componentDidMount() {
+    componentDidMount = () => {
         if (!window.location.hash) {
             document.location.hash = '#hackernews';
         } else {
@@ -46,7 +92,7 @@ class App extends Component {
         window.onhashchange = this.loadData();
     }
 
-    render() {
+    render = () => {
     return (<div>
         <button type="button" className="btn btn-secondary col-2" onClick = {this.loadData}>Reload data</button>
         <div>&nbsp;</div>
